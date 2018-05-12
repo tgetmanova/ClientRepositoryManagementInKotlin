9920bf53b37f1a147577844fbf3251e218da7d76
tgetmanova <tatyana.getmanova@gmail.com>
2018-05-12 17:30:25 +0300

    Introduce manager layer to abstract internal data transformation

---
+++ src/main/java/com/github/spb/tget/demo/ClientManager.java
@@ -0,0 +1,46 @@
+package com.github.spb.tget.demo;
+
+import com.github.spb.tget.demo.converter.ClientConverter;
+import com.github.spb.tget.demo.dao.ClientDao;
+import com.github.spb.tget.demo.dao.ContactDao;
+import com.github.spb.tget.demo.data.Client;
+import com.github.spb.tget.demo.dto.ClientDto;
+import com.github.spb.tget.demo.dto.ContactInformationDto;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ClientManager {
+
+    private ClientDao clientDao = new ClientDao();
+    private ContactDao contactDao = new ContactDao();
+    private ClientConverter clientConverter = new ClientConverter();
+
+
+    public List<ClientDto> getClients() {
+        return clientDao.getClients().stream()
+                .map(client -> clientConverter.toDto(client))
+                .collect(Collectors.toList());
+    }
+
+    public ClientDto getClient(Integer id) {
+        return clientConverter.toDto(clientDao.resolveClient(id));
+    }
+
+    public List<ContactInformationDto> getClientContacts(Integer id) {
+        return clientConverter.toDto(clientDao.resolveClient(id)).getContacts();
+    }
+
+    public void createClient(ClientDto client) {
+        clientDao.createClient(clientConverter.fromDto(client));
+    }
+
+    public void addContacts(List<ContactInformationDto> contacts, Integer id) {
+        Client client = clientDao.resolveClient(id);
+        contactDao.addContacts(contacts.stream().map(c -> clientConverter.contactInfoFromDto(c))
+                .collect(Collectors.toList()), client);
+    }
+}
--- src/main/java/com/github/spb/tget/demo/controller/ContactController.java
+++ src/main/java/com/github/spb/tget/demo/controller/ContactController.java
@@ -1,35 +1,29 @@
 package com.github.spb.tget.demo.controller;
 
 import com.github.spb.tget.demo.ClientManager;
-import com.github.spb.tget.demo.dto.ClientDto;;
+import com.github.spb.tget.demo.dto.ContactInformationDto;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class ClientController {
+public class ContactController {
 
+    private static final String resourceUrl = "/clients/{id}/contacts";
     private ClientManager clientManager = new ClientManager();
 
-    private static final String resourceUrl = "/clients";
-
     @RequestMapping(value = resourceUrl, method = RequestMethod.GET)
-    public List<ClientDto> getClients() {
-        return clientManager.getClients();
-    }
-
-    @RequestMapping(value = resourceUrl + "/{id}", method = RequestMethod.GET)
-    public ClientDto getClient(@PathVariable Integer id) {
-        return clientManager.getClient(id);
+    public List<ContactInformationDto> getClientContacts(@PathVariable Integer id) {
+        return clientManager.getClientContacts(id);
     }
 
     @RequestMapping(value = resourceUrl, method = RequestMethod.POST)
-    public void createClient(@RequestBody ClientDto client) {
-        clientManager.createClient(client);
+    public void addContacts(@RequestBody List<ContactInformationDto> contacts, @PathVariable Integer id) {
+        clientManager.addContacts(contacts, id);
     }
 }
