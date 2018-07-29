fefc7703a343b9c46e6d919e2e6f128f4c69675b
tgetmanova <tatyana.getmanova@gmail.com>
2018-07-29 14:06:12 +0300

    Migrate Country DB Entity

---
+++ Service/src/main/java/com/github/spb/tget/demo/data/CountryEntity.kt
@@ -0,0 +1,8 @@
+package com.github.spb.tget.demo.data
+
+class CountryEntity {
+
+    var name: String? = null
+
+    var code: String? = null
+}
--- Service/src/main/java/com/github/spb/tget/demo/repository/dbRepository/CountryDbRepository.kt
+++ Service/src/main/java/com/github/spb/tget/demo/repository/dbRepository/CountryDbRepository.kt
@@ -1,11 +1,11 @@
 package com.github.spb.tget.demo.repository.dbRepository
 
-import com.github.spb.tget.demo.data.Country
+import com.github.spb.tget.demo.data.CountryEntity
 
-class CountryDbRepository private constructor() : DbRepository<Country>() {
+class CountryDbRepository private constructor() : DbRepository<CountryEntity>() {
 
     init {
-        this.entityName = "Country"
+        this.entityName = "CountryEntity"
     }
 
     companion object {
